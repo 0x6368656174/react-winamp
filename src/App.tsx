18d57b207a5b93657962bc26557e98584eb9ed7b
Pavel Puckov <0x6368656174@gmail.com>
2020-09-07 22:14:51 +1000

    Window drag

--- src/App.tsx
+++ src/App.tsx
@@ -15,54 +15,53 @@ function Main() {
   const [originalMousePosition, setOriginalMousePosition] = useState({ x: 0, y: 0 });
   const [dragged, setDragged] = useState(false);
 
+  const handleMouseMove = (event: React.MouseEvent) => {
+    if (!dragged) {
+      return;
+    }
+    event.preventDefault();
+
+    const mouseEvent = event.nativeEvent;
+    if (!(mouseEvent instanceof MouseEvent)) {
+      throw new Error('Is not mouse event');
+    }
+
+    const moveX = mouseEvent.x - originalMousePosition.x;
+    const moveY = mouseEvent.y - originalMousePosition.y;
+
+    setX(originalPosition.x + moveX);
+    setY(originalPosition.y + moveY);
+  };
+
+  const handleMouseUp = (event: React.MouseEvent) => {
+    if (!dragged) {
+      return;
+    }
+    event.preventDefault();
+
+    setDragged(false);
+  };
+
+  const handleMouseDown = (event: React.MouseEvent) => {
+    event.preventDefault();
+    const mouseEvent = event.nativeEvent;
+    if (!(mouseEvent instanceof MouseEvent)) {
+      throw new Error('Is not mouse event');
+    }
+
+    setOriginalPosition({ x, y });
+    setDragged(true);
+    setOriginalMousePosition({ x: mouseEvent.x, y: mouseEvent.y });
+  };
+
   return (
-    <div
-      className={styles.app}
-      onMouseMove={(event) => {
-        if (!dragged) {
-          return;
-        }
-        event.preventDefault();
-
-        const mouseEvent = event.nativeEvent;
-        if (!(mouseEvent instanceof MouseEvent)) {
-          throw new Error('Is not mouse event');
-        }
-
-        const moveX = mouseEvent.x - originalMousePosition.x;
-        const moveY = mouseEvent.y - originalMousePosition.y;
-
-        setX(originalPosition.x + moveX);
-        setY(originalPosition.y + moveY);
-      }}
-      onMouseUp={(event) => {
-        if (!dragged) {
-          return;
-        }
-        event.preventDefault();
-
-        setDragged(false);
-      }}
-    >
+    <div className={styles.app} onMouseMove={handleMouseMove} onMouseUp={handleMouseUp}>
       <div
         className={styles.player}
         style={{ left: `calc(100vw / 2 - 548px / 2 + ${x}px)`, top: `calc(100px + ${y}px)` }}
       >
         <Player />
-        <div
-          className={styles.dragArea}
-          onMouseDown={(event: React.SyntheticEvent) => {
-            event.preventDefault();
-            const mouseEvent = event.nativeEvent;
-            if (!(mouseEvent instanceof MouseEvent)) {
-              throw new Error('Is not mouse event');
-            }
-
-            setOriginalPosition({ x, y });
-            setDragged(true);
-            setOriginalMousePosition({ x: mouseEvent.x, y: mouseEvent.y });
-          }}
-        />
+        <div className={styles.dragArea} onMouseDown={handleMouseDown} />
         {playlistVisible && <Playlist />}
       </div>
     </div>
