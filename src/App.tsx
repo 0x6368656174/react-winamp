07a7841f0ae68daf86633191a1e80ec430b16f05
Pavel Puckov <0x6368656174@gmail.com>
2020-09-07 22:11:00 +1000

    Window drag

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './App.module.css';
 import { selectPlaylistVisible } from './appSlice';
 import Player from './player/Player';
@@ -9,10 +9,60 @@ import store from './store';
 function Main() {
   const playlistVisible = useSelector(selectPlaylistVisible);
 
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [originalPosition, setOriginalPosition] = useState({ x: 0, y: 0 });
+  const [originalMousePosition, setOriginalMousePosition] = useState({ x: 0, y: 0 });
+  const [dragged, setDragged] = useState(false);
+
   return (
-    <div className={styles.app}>
-      <div className={styles.player}>
+    <div
+      className={styles.app}
+      onMouseMove={(event) => {
+        if (!dragged) {
+          return;
+        }
+        event.preventDefault();
+
+        const mouseEvent = event.nativeEvent;
+        if (!(mouseEvent instanceof MouseEvent)) {
+          throw new Error('Is not mouse event');
+        }
+
+        const moveX = mouseEvent.x - originalMousePosition.x;
+        const moveY = mouseEvent.y - originalMousePosition.y;
+
+        setX(originalPosition.x + moveX);
+        setY(originalPosition.y + moveY);
+      }}
+      onMouseUp={(event) => {
+        if (!dragged) {
+          return;
+        }
+        event.preventDefault();
+
+        setDragged(false);
+      }}
+    >
+      <div
+        className={styles.player}
+        style={{ left: `calc(100vw / 2 - 548px / 2 + ${x}px)`, top: `calc(100px + ${y}px)` }}
+      >
         <Player />
+        <div
+          className={styles.dragArea}
+          onMouseDown={(event: React.SyntheticEvent) => {
+            event.preventDefault();
+            const mouseEvent = event.nativeEvent;
+            if (!(mouseEvent instanceof MouseEvent)) {
+              throw new Error('Is not mouse event');
+            }
+
+            setOriginalPosition({ x, y });
+            setDragged(true);
+            setOriginalMousePosition({ x: mouseEvent.x, y: mouseEvent.y });
+          }}
+        />
         {playlistVisible && <Playlist />}
       </div>
     </div>
